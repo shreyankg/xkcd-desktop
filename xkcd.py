f03c51ae4d27f1a91c5c218d1cc2c6736c29fdeb
Shreyank Gupta <sgupta@redhat.com>
2010-12-23 17:18:12 +0530

    Target xkcd index page. Added word-srap support.

--- xkcd.py
+++ xkcd.py
@@ -25,7 +25,7 @@ from subprocess import call
 import Image, ImageDraw, ImageFont
 
 TMPFILE = "/tmp/xkcd.png"
-XKCD_URL = "http://xkcd.com/835/info.0.json"
+XKCD_URL = "http://xkcd.com/info.0.json"
 
 FONT = "/usr/share/fonts/truetype/Humor-Sans.ttf"
 HEAD = 25
@@ -33,12 +33,28 @@ FOOT = 15
 FORMAT = "PNG"
 TEXT_COLOR = "#000000"
 
+def wrap(text, width):
+    """
+    A word-wrap function that preserves existing line breaks
+    and most spaces in the text. Expects that existing line
+    breaks are posix newlines (\n).
+    """
+    w = reduce(lambda line, word, width=width: '%s%s%s' %
+        (line,
+        ' \n'[(len(line)-line.rfind('\n')-1
+             + len(word.split('\n',1)[0]
+                  ) >= width)],
+        word),
+        text.split(' ')
+    )
+    return w.split('\n')
+
 # Gather JSON data from xkcd.com
 fd = urllib.urlopen(XKCD_URL)
 json_data = json.load(fd)
 url = json_data['img']
 title = json_data['title']
-footer = json_data['alt']
+footer = wrap(json_data['alt'], 80)
 
 # Download image
 call(["wget", 
@@ -51,7 +67,9 @@ header_font = ImageFont.truetype(FONT, HEAD)
 footer_font = ImageFont.truetype(FONT, FOOT)
 
 head_size = header_font.getsize(title)
-foot_size = footer_font.getsize(footer)
+foot_size_single = footer_font.getsize(footer[0])
+foot_size = (foot_size_single[0], 
+    (foot_size_single[1]) * (len(footer)) + (len(footer)) * 5)
 
 xkcd = Image.open(TMPFILE)
 
@@ -62,19 +80,22 @@ new_size = (
 out = Image.new('RGB', new_size, '#ffffff')
 
 head_position = (
-    abs(xkcd.size[0] - head_size[0])/2,
+    (new_size[0] - head_size[0])/2,
     5)
-foot_position = (
-    abs(xkcd.size[0] - foot_size[0])/2,  
-    xkcd.size[1] + head_size[1] + 15)
+foot_position_ver =  xkcd.size[1] + head_size[1] + 5
 
 draw = ImageDraw.Draw(out)
 
 draw.text(head_position, title, font=header_font, fill=TEXT_COLOR)
-draw.text(foot_position, footer, font=footer_font, fill=TEXT_COLOR)
+for foot in footer:
+    internal_foot_size = footer_font.getsize(foot)
+    foot_position_hor = (new_size[0] - internal_foot_size[0])/2
+    foot_position_ver = foot_position_ver + 5 + internal_foot_size[1]
+    foot_position = (foot_position_hor, foot_position_ver)
+    draw.text(foot_position, foot, font=footer_font, fill=TEXT_COLOR)
 
 del draw 
-xkcd_position = (min(0, head_position[0], foot_position[0]),
+xkcd_position = (max(0, (foot_size[0] - xkcd.size[0])/2),
     head_size[1] + 10)
 out.paste(xkcd, xkcd_position)
 out.save(TMPFILE, FORMAT)  
